4bbafce0759bfe86e2e6c9815d7f7c7ad3e76933
AlaaEldeen Ahmed <alaaahmed8383@gmail.com>
2021-11-23 21:07:52 +0400

    push my calculator code

---
+++ Calcolator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Calcolator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
