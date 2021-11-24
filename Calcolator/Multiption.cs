1c9b485c589fadd7feb51e61c85575bcae629891
AlaaEldeen Ahmed <alaaahmed8383@gmail.com>
2021-11-24 09:37:28 +0400

    add mul

---
+++ Calcolator/Multiption.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calcolator
+{
+    class Multiption
+    {
+        public int Mul(int a, int b)
+        {
+            return a * b;
+        }
+    }
+}
