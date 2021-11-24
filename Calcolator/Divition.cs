fbe91f34937cdbef908aa2044d8d4d8090794c58
AlaaEldeen Ahmed <alaaahmed8383@gmail.com>
2021-11-24 09:54:00 +0400

    add div

---
+++ Calcolator/Divition.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calcolator
+{
+    class Divition
+    {
+        public int Div(int a, int b)
+        {
+            int div;
+            div = a / b;
+            return div;
+        }
+    }
+}
